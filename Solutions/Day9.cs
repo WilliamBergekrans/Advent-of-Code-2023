3e0eefcb5beb9d2a6d43fd827dfd2be89a33954d
ewlbrie <william.bergekrans@ericsson.com>
2023-12-12 08:30:20 +0100

    Day 9 part 1 and 2

---
+++ Solutions/Day9.cs
@@ -0,0 +1,58 @@
+﻿namespace AdventOfCode2023.Solutions;
+
+public class Day9: ISolution
+{
+    public int SolutionPartOne(List<string> textInput)
+    {
+        List<List<int>> numberInput = textInput.Select(row =>
+        {
+            return row.Split(' ').Select(c => int.Parse(c.Trim())).ToList();
+        }).ToList();
+        
+        int sum = numberInput.Select(GetNextNumber).Sum();
+        return sum;
+    }
+
+    public int SolutionPartTwo(List<string> textInput)
+    {
+        List<List<int>> numberInput = textInput.Select(row =>
+        {
+            return row.Split(' ').Select(c => int.Parse(c.Trim())).ToList();
+        }).ToList();
+        
+        int sum = numberInput.Select(GetNextNumberPart2).Sum();
+        return sum;
+    }
+
+    private int GetNextNumber(List<int> numbers)
+    {
+        if (numbers.All(n => n == 0))
+        {
+            return 0;
+        }
+
+        List<int> differences = new();
+        for (int i = 0; i < numbers.Count-1; i++)
+        {
+             differences.Add(numbers[i+1] - numbers[i]);
+        }
+
+        return numbers.Last() + GetNextNumber(differences);
+    }
+    
+    private int GetNextNumberPart2(List<int> numbers)
+    {
+        if (numbers.All(n => n == 0))
+        {
+            return 0;
+        }
+
+        List<int> differences = new();
+        for (int i = 0; i < numbers.Count-1; i++)
+        {
+            differences.Add(numbers[i+1] - numbers[i]);
+        }
+
+        return numbers.First() - GetNextNumberPart2(differences);
+    }
+}
